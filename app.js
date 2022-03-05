52355bc50b8658ef49508f860d76eb5cf05c209e
Itcel Lerma <itcel2014@gmail.com>
2022-03-04 23:00:37 -0600

    got name field to work

--- app.js
+++ app.js
@@ -9,49 +9,45 @@ let gameState = {
   turn: "X",
 };
 
-function myFunction(Player1) {
+function myFunction() {
   var x = document.getElementById("myText").value;
   document.getElementById("demo").innerHTML = x;
 }
+function playerTwo() {
+  var x = document.getElementById("Txt").value;
+  document.getElementById("show").innerHTML = x;
 
-// function myFunction(Player2) {
-//   var x = document.getElementById("myText2").value;
-//   document.getElementsByClassName("demo").innerHTML = x;
-// }
+  gameState.board;
+  let currentPlayer = "X";
+  const grid = document.querySelector(".grid");
+  grid.addEventListener("click", function (event) {
+    console.log(event);
 
-gameState.board;
-let currentPlayer = "X";
-const grid = document.querySelector(".grid");
-grid.addEventListener("click", function (event) {
-  console.log(event);
+    if (gameState.count % 2 === 0 && event.target.innerHTML === "") {
+      event.target.innerHTML = "X";
+      gameState.count++;
+    } else if (event.target.innerHTML === "") {
+      event.target.innerHTML = "o";
+      gameState.count++;
+    }
+  });
 
-  if (gameState.count % 2 === 0 && event.target.innerHTML === "") {
-    event.target.innerHTML = "X";
-    gameState.count++;
-  } else if (event.target.innerHTML === "") {
-    event.target.innerHTML = "o";
-    gameState.count++;
+  // keep track of whose turn it is
+  //winner or loser
+  //where x's and o's are
+  // how to check winner
+  function randomizeFirstPlayer() {
+    //randomly determine player one or player two
+    //re-assign the turn to the results of 21
+    gameState.turn = "o";
   }
-});
 
-// keep track of whose turn it is
+  function renderGrid() {}
 
-//winner or loser
-
-//where x's and o's are
-
-// how to check winner
-
-function randomizeFirstPlayer() {
-  //randomly determine player one or player two
-  //re-assign the turn to the results of 21
-  gameState.turn = "o";
-}
-
-function renderGrid() {}
-
-function checkWinner() {
-  //if statement and else if
+  function checkWinner() {
+    //if statement and else if
+  }
+  //add restart button
 }
 
 //add restart button
