33db9f5486194713807383156de4237a5c2ef5e7
Itcel Lerma <itcel2014@gmail.com>
2022-03-06 21:40:44 -0600

    I did my best

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let gameState = {
   players: ["X", "O"],
   board: [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
   ],
   count: 0,
   turn: "X",
 };
 
+//Name fields and buttons
 function myFunction() {
   var x = document.getElementById("myText").value;
   document.getElementById("demo").innerHTML = x;
@@ -16,38 +17,59 @@ function myFunction() {
 function playerTwo() {
   var x = document.getElementById("Txt").value;
   document.getElementById("show").innerHTML = x;
+}
+//x's & o's
+gameState.board;
+let currentPlayer = "X";
+const grid = document.querySelector(".grid");
+grid.addEventListener("click", function (event) {
+  console.log(event);
 
-  gameState.board;
-  let currentPlayer = "X";
-  const grid = document.querySelector(".grid");
-  grid.addEventListener("click", function (event) {
-    console.log(event);
+  if (gameState.count % 2 === 0 && event.target.innerHTML === "") {
+    event.target.innerHTML = "X";
+    gameState.count++;
+  } else if (event.target.innerHTML === "") {
+    event.target.innerHTML = "o";
+    gameState.count++;
+  }
+});
 
-    if (gameState.count % 2 === 0 && event.target.innerHTML === "") {
-      event.target.innerHTML = "X";
-      gameState.count++;
-    } else if (event.target.innerHTML === "") {
-      event.target.innerHTML = "o";
-      gameState.count++;
-    }
-  });
+const playerDisplay = document.querySelector(".displayPlayer");
+const announcer = document.querySelector(".displayWinner");
 
-  // keep track of whose turn it is
+const PlayerXwon = "PLAYER 1 WON!";
+const PlayerOWon = "PLAYER 2 WON!";
+const tieLoser = "it's a tie!";
+
+function renderGrid() {}
+
+function checkWinner() {
+  //if statement and else if
+  //DIDNT WORK DRAFT
+  // function checkWinner(x, o, y) {
+  //   let section = [];
+  //   for (let i = 0; i < x.length; i++)
+  //     if (i < 3) {
+  //       section.push(x[i][0]);
+  //       section.push(x[i][1]);
+  //       section.push(x[i][2]);
+  //     }
+  //   return "WINNER!";
+  // }
+  // console.log(checkWinner(gameState.board, 0, 1, 2));
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   //winner or loser
-  //where x's and o's are
-  // how to check winner
-  function randomizeFirstPlayer() {
-    //randomly determine player one or player two
-    //re-assign the turn to the results of 21
-    gameState.turn = "o";
-  }
-
-  function renderGrid() {}
-
-  function checkWinner() {
-    //if statement and else if
-  }
-  //add restart button
 }
 
-//add restart button
+function createButton() {
+  location.reload();
+}
